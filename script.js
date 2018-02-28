ad9c55c44f783219dbc7d4d79118cf3f628e6a4c
Nicole Theurer <nicole.theurer@outlook.com>
2018-02-28 14:47:37 -0700

    started on sorting hat

---
+++ script.js
@@ -0,0 +1,47 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    house: '',
+  },
+  computed: {
+   houseList: function() {
+     var houseList = new Array;
+     houseList[0] = "Hufflepuff";
+     houseList[1] = "Slytherin";
+     houseList[2] = "Ravenclaw";
+     houseList[3] = "Gryffindor";
+     return month[this.current.month - 1];
+   }
+ },
+  methods: {
+    getRandom: function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      var randomNum = Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive
+      return houseList[randomNum];
+    },
+    sortIntoHouse: function() {
+      this.house = this.getRandom(0, 3);
+      switch(house) {
+        case "Hufflepuff":
+          break;
+        case "Slytherin":
+          break;
+        case "Ravenclaw":
+          break;
+        case "Gryffindor":
+          break;
+      }
+    },
+    // addComment: function() {
+    //   if (!(this.number in this.comments))
+	  //     Vue.set(app.comments, this.number, new Array);
+    //   let currDate = new Date(Date.now());
+    //   this.comments[this.number].push({author:this.addedName,text:this.addedComment,
+    //     date:currDate.getMonth() + "/" + currDate.getDate() + "/" + currDate.getFullYear(),
+    //     time:currDate.getHours() + ":" + currDate.getMinutes()});
+    //   this.addedName = '';
+    //   this.addedComment = '';
+    // },
+  }
+});
